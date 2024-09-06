812196853e610535696ef7706569bfb96cade8bb
Lou <entreoutros@gmail.com>
2024-09-06 19:26:24 -0300

    busca retorna apenas uma instância de cada dado

--- app.js
+++ app.js
@@ -2,41 +2,38 @@ function pesquisar() {
     // Obtém a seção HTML onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
-    let campoPesquisa = document.getElementById("campo-pesquisa").value
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
 
-    // se campoPesquisa for uma string sem nada
+    // Se campoPesquisa for uma string vazia
     if (!campoPesquisa) {
-        section.innerHTML = "<p>Nada foi encontrado</p>"
-        return 
+        section.innerHTML = "<p>Nada foi encontrado</p>";
+        return;
     }
 
-    campoPesquisa = campoPesquisa.toLowerCase()
+    campoPesquisa = campoPesquisa.toLowerCase();
 
     // Inicializa uma string vazia para armazenar os resultados
     let resultados = "";
-    let atributos = ""; 
-    
+
     // Itera sobre cada dado da lista de dados
     for (let dado of dados) {
-        for (let chave of Object.keys(dado)) {
-            // se atributo includes campoPesquisa
-            if (chave.includes(campoPesquisa)) {
-                // cria um novo elemento
-                resultados += `
-                <div class="item-resultado">
-                    <h2>
-                        <a href="#" target="_blank">${dado.titulo}</a>
-                    </h2>
-                    <p class="descricao-meta">${dado.descricao}</p>
-                    <a href=${dado.link} target="_blank">Mais informações</a>
-                </div>
-            `;
-            }
+        // Se algum atributo do dado inclui campoPesquisa
+        if (Object.values(dado).some(value => typeof value === 'string' && value.toLowerCase().includes(campoPesquisa))) {
+            // Cria um novo elemento
+            resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href="${dado.link}" target="_blank">Mais informações</a>
+            </div>
+        `;
         }
     }
 
-    if (!resultados) {
-        resultados = "<p>Nada foi encontrado</p>"
+    if (resultados.length === 0) {
+        resultados = "<p>Nada foi encontrado</p>";
     }
 
     // Atribui os resultados gerados à seção HTML
