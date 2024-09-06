2876ae1d8382e30a0e0b510f12190abc98eae436
Lou <entreoutros@gmail.com>
2024-09-06 18:13:10 -0300

    remove tags

--- app.js
+++ app.js
@@ -16,15 +16,13 @@ function pesquisar() {
     let resultados = "";
     let titulo = ""; 
     let descricao = "";
-    let tags = "";
-
+    
     // Itera sobre cada dado da lista de dados
     for (let dado of dados) {
         titulo = dado.titulo.toLowerCase()
         descricao = dado.descricao.toLowerCase()
-        tags = dado.tags.toLowerCase()
         // se titulo includes campoPesquisa
-        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)) {
             // cria um novo elemento
             resultados += `
             <div class="item-resultado">
